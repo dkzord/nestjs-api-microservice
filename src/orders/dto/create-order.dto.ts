42ea5e51e16b45c6ca3d283be900b97a0c92144f
Fernando Cavalcanti <comododk@gmail.com>
2024-03-15 00:54:12 -0300

    feat: add implement create-order

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,34 @@
-export class CreateOrderDto {}
+import {
+  ArrayNotEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateOrderDto {
+  @ArrayNotEmpty()
+  @Type(() => OrderItemDto)
+  @ValidateNested({ each: true })
+  items: OrderItemDto[];
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  card_hash: string;
+}
+
+export class OrderItemDto {
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  quantity: number;
+
+  @MaxLength(36)
+  @IsString()
+  @IsNotEmpty()
+  product_id: string;
+}
